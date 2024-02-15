7cc243acd8414eb4acf0d98e1905e942d558b5b2
Artem-web-js <zenticul@gmail.com>
2024-02-15 21:00:10 +0200

    init commit

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,11 +1,11 @@
-export default function RootLayout({
-  children,
-}: {
-  children: React.ReactNode;
-}) {
+import { PropsWithChildren } from "react";
+import { inter } from '@/app/ui/fonts';
+import '@/app/ui/global.css';
+
+export default function RootLayout({ children }: PropsWithChildren) {
   return (
     <html lang="en">
-      <body>{children}</body>
+      <body className={`${inter.className} antialiased`}>{children}</body>
     </html>
   );
 }
